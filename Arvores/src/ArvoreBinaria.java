1f47f4362054cb4e8afc6ba9578477e8a4aa2790
Raykkoner Dujhkkovick Silva de Farias <raykkonerd@gmail.com>
2024-08-15 12:32:15 +0000

    Nova apresentação de árvore

--- Arvores/src/ArvoreBinaria.java
+++ Arvores/src/ArvoreBinaria.java
@@ -1,8 +1,6 @@
 import java.lang.Math;
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class ArvoreBinaria {
     private Nodo raiz;
@@ -146,80 +144,106 @@ public class ArvoreBinaria {
         }
     }
 
-    @Override
-    public String toString() {
-        List<List<Nodo>> niveis = nodosPorNivel();
-
-        String str = "";
-        for (int i = 0; i < niveis.size(); i++) {
-            str += "Level " + i + ": ";
-            int nEspacos = (int) (niveis.get(niveis.size() - 1).size() * 2 / Math.pow(2, (i + 1)));
-
-            for (int j = 0; j < nEspacos + niveis.size() - 2 - i; j++) {
-                str += " ";
-            }
-
-            boolean mesmoPai = true;
-            for (Nodo nodo : niveis.get(i)) {
-                str += ((nodo == null) ? "*" : nodo.getValor());
-                for (int k = 0; k < (mesmoPai ? nEspacos * 2 : nEspacos + niveis.size() - 1 - i); k++) {
-                    str += " ";
-                }
-                mesmoPai = !mesmoPai;
-            }
-            str += "\n";
+    public static void printTree(BinaryTree tree) {
+        if (tree == null || tree.getRoot() == null) {
+            System.out.println("The tree is empty.");
+            return;
         }
+        List<List<String>> lines = new ArrayList<>();
+        List<Node> level = new ArrayList<>();
+        List<Node> next = new ArrayList<>();
+        level.add(tree.getRoot());
+        int nn = 1;
+        int widest = 0;
 
-        return str;
-    }
+        while (nn != 0) {
+            List<String> line = new ArrayList<>();
+            nn = 0;
 
-    public List<List<Nodo>> nodosPorNivel() {
-        List<List<Nodo>> niveis = new ArrayList<>();
-        if (this.raiz == null) {
-            return niveis;
-        }
+            for (Node n : level) {
+                if (n == null) {
+                    line.add(null);
 
-        Queue<Nodo> nivel = new LinkedList<>();
-        nivel.add(this.raiz);
-
-        while (!nivelEhUltimo(nivel)) {
-            int nivelTamanho = nivel.size();
-            List<Nodo> currentLevel = new ArrayList<>();
-
-            for (int i = 0; i < nivelTamanho; i++) {
-                Nodo nodo = nivel.poll();
-                currentLevel.add(nodo);
-                if (nodo == null) {
-                    nivel.add(null);
-                    nivel.add(null);
-                    continue;
+                    next.add(null);
+                    next.add(null);
                 } else {
-                    if (nodo.getEsquerda() != null) {
-                        nivel.add(nodo.getEsquerda());
-                    } else {
-                        nivel.add(null);
-                    }
+                    String aa = String.valueOf(n.getValue());
+                    line.add(aa);
+                    if (aa.length() > widest) widest = aa.length();
 
-                    if (nodo.getDireita() != null) {
-                        nivel.add(nodo.getDireita());
-                    } else {
-                        nivel.add(null);
-                    }
+                    next.add(n.getLeft());
+                    next.add(n.getRight());
+
+                    if (n.getLeft() != null) nn++;
+                    if (n.getRight() != null) nn++;
                 }
             }
 
-            niveis.add(currentLevel);
+            if (widest % 2 == 1) widest++;
+
+            lines.add(line);
+
+            List<Node> tmp = level;
+            level = next;
+            next = tmp;
+            next.clear();
         }
 
-        return niveis;
-    }
+        int perpiece = lines.get(lines.size() - 1).size() * (widest + 4);
+        for (int i = 0; i < lines.size(); i++) {
+            List<String> line = lines.get(i);
+            int hpw = (int) Math.floor(perpiece / 2f) - 1;
 
-    public boolean nivelEhUltimo(Queue<Nodo> nivel) {
-        for (Nodo nodo : nivel) {
-            if (nodo != null) {
-                return false;
+            if (i > 0) {
+                for (int j = 0; j < line.size(); j++) {
+                    // split node
+                    char c = ' ';
+                    if (j % 2 == 1) {
+                        if (line.get(j - 1) != null) {
+                            c = (line.get(j) != null) ? '┴' : '┘';
+                        } else {
+                            if (j < line.size() && line.get(j) != null) c = '└';
+                        }
+                    }
+                    System.out.print(c);
+
+                    // lines and spaces
+                    if (line.get(j) == null) {
+                        for (int k = 0; k < perpiece - 1; k++) {
+                            System.out.print(" ");
+                        }
+                    } else {
+                        for (int k = 0; k < hpw; k++) {
+                            System.out.print(j % 2 == 0 ? " " : "─");
+                        }
+                        System.out.print(j % 2 == 0 ? "┌" : "┐");
+                        for (int k = 0; k < hpw; k++) {
+                            System.out.print(j % 2 == 0 ? "─" : " ");
+                        }
+                    }
+                }
+                System.out.println();
             }
+
+            // print line of numbers
+            for (int j = 0; j < line.size(); j++) {
+                String f = line.get(j);
+                if (f == null) f = "";
+                int gap1 = (int) Math.ceil(perpiece / 2f - f.length() / 2f);
+                int gap2 = (int) Math.floor(perpiece / 2f - f.length() / 2f);
+
+                // a number
+                for (int k = 0; k < gap1; k++) {
+                    System.out.print(" ");
+                }
+                System.out.print(f);
+                for (int k = 0; k < gap2; k++) {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+
+            perpiece /= 2;
         }
-        return true;
     }
 }
